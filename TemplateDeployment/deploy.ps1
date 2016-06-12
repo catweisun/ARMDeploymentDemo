0e1c33efb01bee70e05976f42dd23fedef74a10e
Wei Sun <mct_sunwei@hotmail.com>
2016-06-13 00:54:03 +0800

    add github template uri

--- TemplateDeployment/deploy.ps1
+++ TemplateDeployment/deploy.ps1
@@ -1,6 +1,9 @@
 Login-AzureRmAccount -EnvironmentName AzureChinaCloud
 $RGNameNetwork = "RG-ARM-BP-Network"
 $RGNameARMDemoJumpbox ="RG-ARM-Demo-Jumpbox"
+$templateUri = "https://raw.githubusercontent.com/catweisun/ARMDeploymentDemo/master/TemplateDeployment/azuredeploy.json"
+$templateParameterUri ="https://github.com/catweisun/ARMDeploymentDemo/blob/master/TemplateDeployment/azuredeploy.parameters.json"
 $loc = "China North"
 New-AzureRmResourceGroup -Name $RGNameARMDemoJumpbox -Location $loc
+#New-AzureRmResourceGroupDeployment -Name "AzureDemoJumpbox" -ResourceGroupName $RGNameARMDemoJumpbox -TemplateUri $templateUri -TemplateParameterUri $templateParameterUri
 New-AzureRmResourceGroupDeployment -Name "AzureDemoJumpbox" -ResourceGroupName $RGNameARMDemoJumpbox -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
