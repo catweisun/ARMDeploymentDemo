0cda6e4242b734e248b45b738cdeeb154ac249af
wei sun <wei sun>
2016-06-27 23:44:00 +0800

    Add virtual network support in jumpbox

--- TemplateDeployment/deploy.ps1
+++ TemplateDeployment/deploy.ps1
@@ -1,13 +1,13 @@
 Login-AzureRmAccount -EnvironmentName AzureChinaCloud
-$RGNameNetwork = "RG-ARM-BP-Network"
 $RGNameARMDemoJumpbox ="RG-ARM-Demo-Jumpbox"
 $templateUri = "https://raw.githubusercontent.com/catweisun/ARMDeploymentDemo/master/TemplateDeployment/azuredeploy.json"
 $templateParameterUri ="https://github.com/catweisun/ARMDeploymentDemo/blob/master/TemplateDeployment/azuredeploy.parameters.json"
 $loc = "China North"
+Select-AzureRmSubscription -SubscriptionId 27621395-3c2d-47e0-9734-102ef2b38b08 
 New-AzureRmResourceGroup -Name $RGNameARMDemoJumpbox -Location $loc -Force
 $beginTime = Get-Date
 #New-AzureRmResourceGroupDeployment -Name "AzureDemoJumpbox" -ResourceGroupName $RGNameARMDemoJumpbox -TemplateUri $templateUri -TemplateParameterUri $templateParameterUri
-New-AzureRmResourceGroupDeployment -Name "AzureDemoJumpbox" -ResourceGroupName $RGNameARMDemoJumpbox -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
+New-AzureRmResourceGroupDeployment -Name "AzureDemoJumpboxDeployment" -ResourceGroupName $RGNameARMDemoJumpbox -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
 $endTime = Get-Date
 
 $totalSeconds = ($endTIme-$beginTime).TotalSeconds
